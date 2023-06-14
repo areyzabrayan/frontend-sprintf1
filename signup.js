09446ce4f9849c3a66337b25ab167223a0562778
Alexis Garcia <alexis25yo@gmail.com>
2023-06-14 16:23:09 -0500

    up solution

--- signup.js
+++ signup.js
@@ -1,17 +1,14 @@
 const form = document.getElementById("form");
 
-const createUser = (form) =>{
-    const newUser = {};    
-    const newUserInput = form.querySelectorAll(".input input");
-    Array.from(newUserInput).forEach((item) =>{
-        newUser[item.id] = item.value;
-    });
-    console.log(newUser); 
-
+const createUser = (form) => {
+  const newUser = {};
+  const newUserInput = form.querySelectorAll(".input input");
+  Array.from(newUserInput).forEach((item) => {
+    newUser[item.id] = item.value;
+  });
+  console.log(newUser);
 };
-form.addEventListener("submit", (e) =>{
-    e.preventDefault();
-    createUser(form);
-    
-})
- 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  createUser(form);
+});
