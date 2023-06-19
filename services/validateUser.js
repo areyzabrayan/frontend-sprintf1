4995ebf15887d6afc4b52ca9bf1fec3847a3bf52
Brayan Areiza <areizabrayan11@gmail.com>
2023-06-19 15:28:00 -0500

    crete user in rlocal

--- services/validateUser.js
+++ services/validateUser.js
@@ -49,3 +49,52 @@ async function validateUser(event) {
 }
 
 export default validateUser;
+
+//--- VALIDACION DE USUARIO
+
+const form = document.querySelector("formulario");
+
+const createUser = async (form) => {
+    try {
+      const response = await axios.get(URL_API);
+      const usuarios = response.data;
+  
+      const newUser = {};
+      const newUserInput = form.querySelectorAll(".cuenta-gratis input");
+      Array.from(newUserInput).forEach((item) => {
+        newUser[item.id] = item.value;
+      });
+  
+      const nameExists = usuarios.some((user) => user.Nombre === newUser.name);
+      if (nameExists) {
+        alert("El nombre ya existe");
+        return;
+      }
+     const numberExists = usuarios.some((user) => user.Celphone === newUser.number);
+      if (numberExists) {
+        alert("Número de celular ya registrado");
+        return;
+      }
+  
+      const passwordExists = usuarios.some((user) => user.password === newUser.password);
+      if (passwordExists) {
+        alert("La contraseña ya existe");
+        return;
+      }
+  
+      const urlExists = usuarios.some((user) => user.url === newUser.url);
+      if (urlExists) {
+        alert("La URL de imagen ya existe");
+        return;
+      }
+  
+      console.log(newUser);
+      form.reset();
+    } catch (error) {
+      console.log(error);
+      alert("Ha ocurrido un error al crear el usuario");
+    }
+  };
+  
+createUser(form);
+
