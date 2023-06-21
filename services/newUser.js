51837a18fe5051ae03b87278b40d65af943dd8ee
Brayan Areiza <areizabrayan11@gmail.com>
2023-06-21 14:22:00 -0500

    funtion newuser progess

--- services/newUser.js
+++ services/newUser.js
@@ -1,59 +1,136 @@
 // import { URL_API } from "../services/dataUsers.js";
+// import getUsers from "../services/getUsers.js";
+// import {
+//   popNotification,
+//   popNotification2,
+// } from "../modules/popNotification.js";
+// import {
+//   form3,
+//   nameR,
+//   celphoneR,
+//   passwordR,
+//   urlR
+// } from "../modules/dataDom.js";
 
-// const form = document.getElementsByClassName("formulario")[0];
+// const newUser = async (event) => {
+//   event.preventDefault();
+  
 
-// const createUser = async (form) => {
 //   try {
-//     const response = await axios.get(URL_API);
-//     const usuarios = response.data;
+    
+//     const users = await getUsers(URL_API);
+   
+//     console.log(users);
 
-//     const newUser = {};
-//     const newUserInputs = form.querySelectorAll(".input input");
-//     Array.from(newUserInputs).map((item) => {
-//       newUser[item.id] = item.value;
-//     });
+//     const newUser={};
 
-//     const userExists = usuarios.find(
-//       (user) =>
-//         user.Nombre === newUser.name ||
-//         user.Celphone === newUser.Celphone ||
-//         user.password === newUser.password
-//     );
-//     if (userExists) {
-//       alert('Usuario ya existe');
+//     const newCelphoneR = celphoneR.value;
+
+//     const foundNumber = users.find((user) => user.Celphone == newCelphoneR);
+//     if (foundNumber) {
+
+//       popNotification("Numero existente");
+//       // location.reload();
 //       return;
-//     }
+//     } 
+//       console.log(newUser);
+//       form3.reset();
 
-//     console.log(newUser);
-//     form.reset();
-
-//     addToUsers(newUser);
+//       addToUsers(newUser);
 //   } catch (error) {
 //     console.log(error);
-//     alert("Ha ocurrido un error al crear el usuario");
-//   },
-// }
+//   }
+// };
 
 // const addToUsers = (newUser) => {
-//   axios
-//     .post(URL_API, newUser, {
+//   axios.post(URL_API, newUser, {
 //       headers: {
 //         'Content-Type': 'application/json',
 //       },
 //     })
 //     .then((response) => {
-//       alert('Elemento agregado a Usuarios: ' + response.data);
+//       console.log(response);
+//       alert('Elemento agregado a Usuarios:', response.data);
 //     })
 //     .catch((error) => {
 //       console.log('Error al agregar elemento a Usuarios:', error);
 //     });
 // };
 
-// async function newUser(event) {
-//   event.preventDefault();
-//   await createUser(form);
-// }
+//   export default newUser;
 
-// export default newUser;
+//PRUEBA 6
+
+import { URL_API } from "../services/dataUsers.js";
+import getUsers from "../services/getUsers.js";
+import {
+  popNotification,
+  popNotification2,
+} from "../modules/popNotification.js";
+import {
+  form3,
+  nameR,
+  celphoneR,
+  passwordR,
+  urlR
+} from "../modules/dataDom.js";
+
+const newUser = async (event) => {
+  event.preventDefault();
+  
+
+  try {
+    
+    const users = await getUsers(URL_API);
+   
+    console.log(users);
+
+    const newUser={};
+    // const newUserInput = form.querySelectorAll("#form3 input");
+    // Array.from(newUserInput).forEach((item) => {
+    //   newUser[item.id] = item.value;
+    // });
+  
+
+    
+    const newCelphoneR = celphoneR.value;
+
+    const foundNumber = users.find((user) => user.Celphone == newCelphoneR);
+    if (foundNumber) {
+
+      popNotification("Numero existente");
+      // location.reload();
+      return;
+    } 
+    const newNmane = nameR.value;
+    const newPassword = passwordR.value;
+    const newUrl = urlR.value;
+    console.log(newPassword);
+      console.log(newUser);
+      form3.reset();
+
+      addToUsers(newUser);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const addToUsers = (newUser) => {
+  axios.post(URL_API, newUser, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    .then((response) => {
+      console.log(response);
+      alert('Elemento agregado a Usuarios:', response.data);
+    })
+    .catch((error) => {
+      console.log('Error al agregar elemento a Usuarios:', error);
+    });
+};
+
+  export default newUser;
 
 
+ 
