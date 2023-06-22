f931d3ee4e71e7a88cb7e7ac2cfe090e214fffd0
Alexis Garcia <alexis25yo@gmail.com>
2023-06-22 14:12:13 -0500

    add changes

--- modules/printContacts.js
+++ modules/printContacts.js
@@ -17,54 +17,29 @@ const printPersons = (array, container) => {
   array.forEach((item) => {
     console.log(item.Nombre);
     console.log(item.info);
-
     const card = document.createElement("div");
     card.classList.add("block");
-
-    const imgchat = document.createElement("figure");
-    imgchat.classList.add("imgchat");
-
-    const img = document.createElement("img");
-    img.src = item.Url_image;
-    img.alt = "imgchat";
-    img.classList.add("cover");
-
-    imgchat.appendChild(img);
-    card.appendChild(imgchat);
-
-    const description = document.createElement("div");
-    description.classList.add("description");
-
-    const headList = document.createElement("div");
-    headList.classList.add("headList");
-
-    const name = document.createElement("h4");
-    name.textContent = item.Nombre;
-
-    const time = document.createElement("p");
-    time.classList.add("time");
-    time.textContent = "12:27";
-
-    headList.appendChild(name);
-    headList.appendChild(time);
-
-    const message = document.createElement("div");
-    message.classList.add("message");
-
-    const p = document.createElement("p");
-    p.textContent = item.info;
-
-    const b = document.createElement("b");
-    b.textContent = "1";
-
-    message.appendChild(p);
-    message.appendChild(b);
-
-    description.appendChild(headList);
-    description.appendChild(message);
-
-    card.appendChild(description);
-
+    card.innerHTML = `
+      <figure class="imgchat">
+        <img
+          src="${item.Url_image}"
+          alt="imgchat"
+          class="cover"
+        />
+      </figure>
+      <div class="description">
+        <div class="headList">
+          <h4>${item.Nombre}</h4>
+          <p class="time">12:27</p>
+        </div>
+        <div class="message">
+          <p>
+            ${item.info}
+          </p>
+          <b>1</b>
+        </div>
+      </div>    
+    `;
     container.appendChild(card);
   });
 };
